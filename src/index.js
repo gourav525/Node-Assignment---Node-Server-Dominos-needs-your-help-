c0eb919288393595ab571a27fdd5c1bfa7b56268
gourav <gouravthakur525@gmail.com>
2021-06-03 16:50:02 +0530

    new code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 var http = require("http");
+var url = require("url");
 
 const httpServer = http.createServer(handleServer);
 
-
-function handleServer(req, res) {
-  
+var data = {
+    phone : '18602100000',
+    email : 'guestcaredominos@jublfood.com'
 }
 
+function handleServer(req, res) {
+    if(req.url == "/welcome"){
+        res.writeHead(200);
+        res.end("Welcome to Dominos!");
+    }else if(req.url == "/contact"){
+        res.writeHead(200);
+        res.end(data.toString());
+    }else{
+        res.writeHead(404);
+    }
+}
+httpServer.listen(8001);
 module.exports = httpServer;
